cbcca23bd600fbc5275004d2505a39a1b19532dd
kib <kib@macbook-pro-de-kib.home>
2020-04-03 16:39:39 +0200

    here we go with konami

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('bwop')
+module.exports = function konami (cb) {
+    let currentIndex = 0
+    const konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+    document.addEventListener('keyup', listenKonami, false)
+
+    function listenKonami(e) {
+        const keyCode = e.keyCode
+        const isKeyCodeValid = keyCode === konami[currentIndex]
+
+        isKeyCodeValid ? currentIndex++ : currentIndex = 0
+
+        if (currentIndex === konami.length) {
+            cb()
+            document.removeEventListener('keyup', listenKonami, false)
+        }
+    }
+}
